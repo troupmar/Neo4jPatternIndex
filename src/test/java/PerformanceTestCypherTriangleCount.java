58ee0cb03b39758b30b2a6e7106f7f708e86e609
Martin Troup <troupmar@gmail.com>
2015-03-29 14:46:17 +0200

    Just checking git sync.

--- src/test/java/PerformanceTestCypherTriangleCount.java
+++ src/test/java/PerformanceTestCypherTriangleCount.java
@@ -21,11 +21,9 @@ public class PerformanceTestCypherTriangleCount implements PerformanceTest {
     /*
     * 1) dotazovat se nad 1 id nodu a unionovat to pres počet uzlu
     *   - chytristika nad neopakováním se pro dotazovaní nad jedním uzlem, již dotazované uzly neopakovat a rovnou přeskočit
-    *   
     * 2) to samé, ale dotazovat se přes všechny nody a udělat faktorial počtu nodu = počet unionu
     * 3) vytvoření externí databaze a po jednom patternu to tam šoupat a dotazovat se nad tím, pak to smazat a takhle dokola krom vytvoreni DB
     * 4) všechno to samé jako předchozí ale na začátku vytvořit DB a všechny patterny tam vložit = subgraf patternu a nad tím se pak dotazovat přes všechny zaindexpvané patterny
-    *
     * */
     /**
      * {@inheritDoc}
