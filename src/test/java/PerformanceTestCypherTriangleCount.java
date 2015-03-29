e7355dbb0fda2f44504442002a38477b0ec29b7f
Martin Troup <troupmar@gmail.com>
2015-03-29 14:49:23 +0200

    Just checking git sync.

--- src/test/java/PerformanceTestCypherTriangleCount.java
+++ src/test/java/PerformanceTestCypherTriangleCount.java
@@ -24,7 +24,7 @@ public class PerformanceTestCypherTriangleCount implements PerformanceTest {
     * 2) to samé, ale dotazovat se přes všechny nody a udělat faktorial počtu nodu = počet unionu
     * 3) vytvoření externí databaze a po jednom patternu to tam šoupat a dotazovat se nad tím, pak to smazat a takhle dokola krom vytvoreni DB
     * 4) všechno to samé jako předchozí ale na začátku vytvořit DB a všechny patterny tam vložit = subgraf patternu a nad tím se pak dotazovat přes všechny zaindexpvané patterny
-    * */
+    *
     /**
      * {@inheritDoc}
      */
